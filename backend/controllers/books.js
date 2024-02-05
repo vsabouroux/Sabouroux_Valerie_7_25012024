750d789c2829f7c26ae852d056cc3fecf9b64d87
vsabouroux <vsabouroux@aol.com>
2024-02-05 13:53:50 +0100

    fonction modifier, supprimer, obtenir un livre en cours

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,4 +1,4 @@
-const Book = require('../models/Book');
+const Book = require("../models/Book");
 
 exports.createBook = (req, res, next) => {
   const bookObjet = JSON.parse(req.body.book);
@@ -7,32 +7,101 @@ exports.createBook = (req, res, next) => {
   const book = new Book({
     ...bookObjet,
     userId: req.auth.userId,
-    imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
   });
-  book.save().then(
-    () => {
+  book.save()
+    .then(() => {
       res.status(201).json({
-        message: 'Livre créé !'
+        message: "Livre créé !",
       });
-    }
-  ).catch(
-    (error) => {
+    })
+    .catch((error) => {
       res.status(400).json({
-        error: error
+        error: error,
       });
-    }
-  );
+    });
 };
 exports.getAllBooks = (req, res, next) => {
-    Book.find().then(
-      (books) => {
-        res.status(200).json(books);
+  Book.find()
+    .then((books) => {
+      res.status(200).json(books);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+};
+
+exports.getOneBook = (req, res, next) => {
+
+  Book.findOne({
+    _id: req.params.id,
+    // userId: req.auth.userId,
+  })
+    .then((book) => {
+      if (!book) {
+        return res.status(404).json({ message: "Livre non trouvé !" });
       }
-    ).catch(
-      (error) => {
-        res.status(400).json({
-          error: error
+      res.status(200).json(book);
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
+};
+
+exports.modifyBook = (req, res, next) => {
+  const bookObjet = req.file
+    ? {
+        ...JSON.parse(req.body.book),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }
+    : { ...req.body };
+  delete bookObjet._userId;
+
+  Book.findOne({ _id: req.params.id })
+    .then((book) => {
+      if (!book) {
+        return res.status(404).json({ message: "Livre non trouvé !" });
+      }
+      if (book.userId !== req.auth.userId) {
+        res.status(401).json({ message: "Accès non authorisé !" });
+      } else {
+        Book.updateOne(
+          { _id: req.params.id },
+          { ...bookObjet, _id: req.params.id }
+        )
+          .then(() => res.status(200).json({ message: "Livre modifié !" }))
+          .catch((error) => res.status(500).json({ error }));
+      }
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
+};
+
+exports.deleteBook = (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then((book) => {
+      if (!book) {
+        return res.status(404).json({ message: "Livre non trouvé !" });
+      }
+      if (book.userId !== req.auth.userId) {
+        return res.status(401).json({ message: "Accès non autorisé !" });
+      }
+      Book.deleteOne({ _id: req.params.id })
+        .then(() => {
+          res.status(200).json({ message: "Livre supprimé !" });
+        })
+        .catch((error) => {
+          res.status(500).json({ error });
         });
-      }
-    );
-  };
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
+};
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const router = express.Router();// méthode Router d'express
+const router = express.Router(); // méthode Router d'express
 //"/api/books" = route de base url de base donc on va le remplacer par /
 const auth = require("../middleware/auth");
-const multer = require("..middleware/multer-config");
+const multer = require("../middleware/multer-config");
 const booksCtrl = require("../controllers/books");
 
 router.get("/", booksCtrl.getAllBooks);
+router.get("/:id", booksCtrl.getOneBook);
 router.post("/", auth, multer, booksCtrl.createBook);
+router.put("/:id", auth, multer, booksCtrl.modifyBook)
+router.delete("/:id", auth, booksCtrl.deleteBook);
 
 
 module.exports = router;
